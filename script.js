3e3b07ca2934d8a42d875744e92a4b376db2d1d0
Robson Camacho <robson-camacho@hotmail.com>
2024-08-15 23:56:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function calcular(event) {
+    event.preventDefault();
+
+    let alcoolInput = document.getElementById("alcool").value;
+    let gasolinaInput = document.getElementById("gasolina").value;
+    let contentResult = document.getElementById("content-result");
+    let textResult = document.getElementById("text-result")
+
+    let gasolinaSpan = document.getElementById("gasolina-result");
+    let alcoolSpan = document.getElementById("alcool-result");
+
+    let calculo = (alcoolInput / gasolinaInput);
+
+    if (calculo < 0.7) {
+        // Aqui compensa usar álcool
+        textResult.innerHTML = "Compensa usar Álcool";
+    }
+    else {
+        //Compensa usar gasolina
+        textResult.innerHTML = "Compensa usar Gasolina";
+    }
+
+    gasolinaSpan.innerHTML = "Gasolina R$ " + gasolinaInput;
+    alcoolSpan.innerHTML = "Álcool R$ " + alcoolInput;
+
+    contentResult.classList.remove("hide")
+}
